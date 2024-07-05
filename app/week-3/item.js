789afea51073e9c2a3ac241a20de4a330c31b30b
Darrel Nguyen <darrelnguyen03@gmail.com>
2024-07-05 16:56:34 -0600

    week 3

---
+++ app/week-3/item.js
@@ -0,0 +1,12 @@
+export default function Item({name, quantity, category}) {
+    return(
+        <div>
+            
+            <h1 className="border-gray-500 bg-gray-400 w-full max-w-xs m-4 p-2">
+                <h2 className="font-bold text-xl">{name}</h2>
+                <p>Get {quantity} from {category}</p>
+            </h1>
+            
+        </div>
+    );
+}
---
+++ app/week-3/page.js
@@ -0,0 +1,15 @@
+import ItemList from "./item-list";
+
+export default function Week3() {
+    return (
+        
+        <main>
+            <body className="h-screen bg-gradient-to-b bg-gray-600"></body>
+            <h1 className="font-bold text-3xl">
+                Shopping List
+            </h1>
+            <ItemList />
+        </main>
+    );    
+}
+                        
