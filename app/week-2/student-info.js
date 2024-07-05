fd59882c07e0bde70379229870a9a6ae3fd0316e
Darrel Nguyen <darrelnguyen03@gmail.com>
2024-07-05 15:26:39 -0600

    Week 2

---
+++ app/week-2/student-info.js
@@ -0,0 +1,15 @@
+import Link from "next/link";
+
+export default function StudentInfo() {
+  return (
+        <div>
+        <p>Darrel Nguyen</p>
+        <p>
+            <Link href="https://github.com/DarrelNugget">GitHub</Link>
+        </p>
+        </div>
+  );
+}
+
+
+
