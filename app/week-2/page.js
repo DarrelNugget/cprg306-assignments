aa2427076301e98f01d20fede3bfdc6d1d677af0
Darrel Nguyen <darrelnguyen03@gmail.com>
2024-07-05 15:27:51 -0600

    Week 2 Cleanup

--- app/week-2/page.js
+++ app/week-2/page.js
@@ -1,14 +1,10 @@
 import StudentInfo from "./student-info";
-import Link from "next/link";
 
 export default function Page() {
     return (
         <main>
             <h1>Shopping List</h1>
             <StudentInfo />
-            <div>
-            
-            </div>
         </main>
     );
     }
